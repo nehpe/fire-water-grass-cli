e99a5be129d78f5b9a325241592df6cb64084eef
nehpe <n@nehpe.com>
2023-11-15 15:20:48 -0700

    Full working game, albeit short af

--- cmd/fwg/main.go
+++ cmd/fwg/main.go
@@ -1,5 +1,32 @@
-package fwg
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/onwhee/go-fwg/pkg/game"
+)
 
 func main() {
+  args := os.Args[1:]
+  choice := args[0]
+  fmt.Println("Choice: "+choice)
+
+  g := game.New()
+  g.UserChoice(choice)
+  g.Choose()
+  g.Decide()
+
+  if g.Draw {
+    fmt.Println("Your game ended in a draw.")
+  } else {
+    if g.Decision == game.WINNER_PLAYER {
+      fmt.Printf("You won! (%s vs. %s)", g.PlayerChoice, g.CpuChoice)
+    } else {
+      fmt.Printf("You lost! (%s vs. %s)", g.PlayerChoice, g.CpuChoice)
+    }
+  }
+  fmt.Println()
+
 
 }
